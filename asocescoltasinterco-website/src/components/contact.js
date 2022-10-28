f227c612306e8558a3c15b753916e9eb5542989a
Erickln <erickelnd@gmail.com>
2022-10-28 17:39:20 -0500

    Contact almost done

--- asocescoltasinterco-website/src/components/contact.js
+++ asocescoltasinterco-website/src/components/contact.js
@@ -6,36 +6,22 @@ import "../styles/contact.css";
 export default function contact() {
   return (
     <div className="contact">
-      <div style={{ textAlign: "center" }}>Hola 2</div>
-
       <div class="contenedorcontactos">
-        <div class="contactanos">
-          Contactanos
-        </div>
-        <div class = "lineacontacto">
-          <hr>
-          </hr>
+        <div class="contactanos">Contactanos</div>
+        <div class="lineacontacto">
+          <hr></hr>
         </div>
         <header class="TenlaConfianza">
-          Ten la confianza de contactarnos mediante cualquiera de los medios presentados a continuación para tratar el tema en el cual estás interesado, trateremos de responder lo más pronto posible.
+          Ten la confianza de contactarnos mediante cualquiera de los medios
+          presentados a continuación para tratar el tema en el cual estás
+          interesado, trateremos de responder lo más pronto posible.
         </header>
-        <div class="contactoCorreo">
-          Hola
-        </div>
-        <div class="contactoFacebook">
-          Hola
-        </div>
-        <div class="contactoWhatsapp">
-          Hola
-        </div>
-        <div class="contactoLinkedIn">
-          Hola
-        </div>
-        <div class="contactoInstagram">
-          Hola
-        </div>
-        
-
+        <div class="contactoCorreo">Correo</div>
+        <div class="contactoFacebook">Facebook</div>
+        <div class="contactoWhatsapp">WhatsApp</div>
+        <div class="contactoLinkedIn">LinkedIn</div>
+        <div class="contactoInstagram">Instagram</div>
+        {/* <div class="contactoCorreo2">Correo</div> */}
       </div>
     </div>
   );
