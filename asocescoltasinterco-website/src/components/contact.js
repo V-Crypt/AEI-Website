27745435f793e640300b2109932f639fc12084d4
V-Crypt <js_monterrubio.ec@outlook.com>
2022-10-29 00:18:30 -0500

    Avances de medio término

--- asocescoltasinterco-website/src/components/contact.js
+++ asocescoltasinterco-website/src/components/contact.js
@@ -1,28 +1,58 @@
-import React from "react";
-
-import { Grid, Image, Segment } from "semantic-ui-react";
+import React, { Component } from "react";
 import "../styles/contact.css";
 
-export default function contact() {
-  return (
-    <div className="contact">
-      <div class="contenedorcontactos">
-        <div class="contactanos">Contactanos</div>
-        <div class="lineacontacto">
-          <hr></hr>
+export default class footer extends Component {
+  render() {
+    return (
+      <div className="contact">
+        <div className="primerDiv">
+          <div class="contenedorcontactos">
+            <div class="contactanos">
+              <h1>Contáctanos</h1>
+              <div class="lineacontacto">
+                <hr />
+              </div>
+              <p>
+                Ten la confianza de contactarnos mediante cualquiera de los medios
+                presentados a continuación para tratar el tema en el cual estás
+                interesado, trateremos de responder lo más pronto posible.
+              </p>
+            </div>
+          </div>
         </div>
-        <header class="TenlaConfianza">
-          Ten la confianza de contactarnos mediante cualquiera de los medios
-          presentados a continuación para tratar el tema en el cual estás
-          interesado, trateremos de responder lo más pronto posible.
-        </header>
-        <div class="contactoCorreo">Correo</div>
-        <div class="contactoFacebook">Facebook</div>
-        <div class="contactoWhatsapp">WhatsApp</div>
-        <div class="contactoLinkedIn">LinkedIn</div>
-        <div class="contactoInstagram">Instagram</div>
-        {/* <div class="contactoCorreo2">Correo</div> */}
+        <div id='info'>
+          <div id="contactoCorreo">
+            <h2>Correo</h2>
+            <p>
+              asoc_escoltasinterco@outlook.com
+            </p>
+          </div>
+          <div id="contactoFacebook">
+            <h2>Facebook</h2>
+            <p>
+              Asociación de Escoltas Intercontinentales
+            </p>
+          </div>
+          <div id="contactoWhatsapp">
+            <h2>WhatsApp</h2>
+            <p>
+              +52 1 33 3468 6232
+            </p>
+          </div>
+          <div id="contactoLinkedIn">
+            <h2>LinkedIn</h2>
+            <p>
+              Asociación de Escoltas Intercontinentales
+            </p>
+          </div>
+          <div id="contactoInstagram">
+            <h2>Instagram</h2>
+            <p>
+              asoc_escoltasinterco
+            </p>
+          </div>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
